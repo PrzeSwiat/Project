a943bf250c9d6499459aadadcc758038327e1a63
236671 Swiatek Przemyslaw <236671@edu.p.lodz.pl>
2022-03-24 13:50:48 +0100

    First_setup

---
+++ Projekt/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Projekt
+{
+    public class Class1
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ Testing/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projekt;
+namespace Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Class1 class1 = new Class1();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(class1.add(2, 3), 5);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(class1.subtract(1,19), -18);
+        }
+    }
+}
