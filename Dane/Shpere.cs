4a98a28b75d7b8334184e648e531532317fda2af
Przemysław Świątek <236671@edu.p.lodz.pl>
2022-04-26 23:03:42 +0200

    impelmentation of sphere

--- Dane/Shpere.cs
+++ Dane/Shpere.cs
@@ -8,6 +8,29 @@ namespace Dane
 {
     internal class Shpere
     {
+        private double Radius { get; set; }
+        private double PositionX { get; set; }
+        private double PositionY { get; set; }
+        private double SpeedX { get; set; }
+        private double SpeedY { get; set; }
+
+        public Shpere(double positionX, double positionY, double speedX, double speedY, double radius)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+            SpeedX = speedX;
+            SpeedY = speedY;
+            Radius = radius;
+        }
+
+        public void Move(double milliseconds)
+        {
+            PositionX += SpeedX * milliseconds;
+            PositionY += SpeedY * milliseconds;
+        }
 
     }
+
+    
+
 }
