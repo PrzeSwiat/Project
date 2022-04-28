5f4c503008c1c8c22b06041a2cddc093904d9210
Przemysław Świątek <236671@edu.p.lodz.pl>
2022-04-28 20:42:17 +0200

    poprawki

---
+++ Dane/Shapes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    public abstract class Shapes
+    {
+        private double PositionX { get; set; }
+        private double PositionY { get; set; }
+        private double VectorX { get; set; }
+        private double VectorY { get; set; }
+    }
+    internal class Sphere : Shapes
+    {
+        private double Radius { get; set; }
+        private double PositionX { get; set; }
+        private double PositionY { get; set; }
+        private double VectorX { get; set; }
+        private double VectorY { get; set; }
+
+        public Sphere(double positionX, double positionY, double radius)
+        {
+            Radius = radius;
+            PositionX = positionX;
+            PositionY = positionY;
+            
+        }
+
+        public void Move(double vectorX, double vectorY)
+        {
+            VectorX = vectorX;
+            VectorY = vectorY;
+            PositionX += VectorX;
+            PositionY += VectorY;
+        }
+
+
+
+    }
+
+    
+
+}
